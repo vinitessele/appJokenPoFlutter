907848e705770e90d8d070070afdbc3f302128ff
vinitessele <vinicius_tessele@hotmail.com>
2023-10-04 22:48:49 -0300

    finalização

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 
@@ -7,11 +8,54 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final _imagemApp = AssetImage("images/padrao.png");
-  final _mensagem = "Escolha uma opção abaixo";
+  var _imagemApp = AssetImage("images/padrao.png");
+  var _mensagem = "Escolha uma opção abaixo";
   var _userWin = 0;
   var _appWin = 0;
   var _empate = 0;
+
+  void _opcaoSelecionada(String escolhaUsuario) {
+    var opcoes = ["pedra", "papel", "tesoura"];
+    var numero = Random().nextInt(3);
+    var escolhaApp = opcoes[numero];
+
+    switch (escolhaApp) {
+      case "pedra":
+        setState(() {
+          _imagemApp = AssetImage("images/pedra.png");
+        });
+        break;
+      case "papel":
+        setState(() {
+          _imagemApp = AssetImage("images/papel.png");
+        });
+        break;
+      case "tesoura":
+        setState(() {
+          _imagemApp = AssetImage("images/tesoura.png");
+        });
+    }
+    if ((escolhaUsuario == "pedra" && escolhaApp == "tesoura") ||
+        (escolhaUsuario == "tesoura" && escolhaApp == "papel") ||
+        (escolhaUsuario == "papel" && escolhaApp == "pedra")) {
+      setState(() {
+        _mensagem = "Parabéns!!! Você ganhou :)";
+        _userWin += 1;
+      });
+    } else if ((escolhaUsuario == "tesoura" && escolhaApp == "pedra") ||
+        (escolhaUsuario == "papel" && escolhaApp == "tesoura") ||
+        (escolhaUsuario == "pedra" && escolhaApp == "papel")) {
+      setState(() {
+        _mensagem = "Você perdeu :(";
+        _appWin += 1;
+      });
+    } else {
+      setState(() {
+        _mensagem = "Empatamos ;)";
+        _empate += 1;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
